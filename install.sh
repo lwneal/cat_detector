75ac359da730b030bf1f759e413a9af7dd02491c
Lawrence Neal <nealla@lwneal.com>
2016-04-05 19:48:06 -0700

    Add script to install dependencies

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: This would all be great to have in a virtualenv
+
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo apt-get install python-scipy
+sudo apt-get install python-opencv
+sudo apt-get install python-h5py
+
+sudo pip install keras
+sudo pip install flask
